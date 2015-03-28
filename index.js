389c2679fcb9dc37d67de831d4b744aa50ca239c
Andre Alves Garzia <contato@andregarzia.com>
2015-03-28 20:48:27 +0100

    testing

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
+var bodyParser = require('body-parser');
 var db = require('pg-db')();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 
 app.get('/comments', function (request, response) {
